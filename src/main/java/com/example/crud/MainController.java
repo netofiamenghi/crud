299e084a8ada18a6198ebf1e1047fcaad06855f8
antonioneto <neto.fiamenghi@gmail.com>
2023-10-08 23:05:40 -0300

    Read User

--- src/main/java/com/example/crud/MainController.java
+++ src/main/java/com/example/crud/MainController.java
@@ -1,7 +1,6 @@
 package com.example.crud;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/crud")
@@ -48,17 +46,17 @@ public class MainController {
 		return "Deleted";
 	}
 	
-	@GetMapping(path = "/retrieve")
-	public @ResponseBody Optional<User> retrieveUser(@RequestParam Integer id) {
-		return userRepository.findById(id);
+	@GetMapping(value = "read/{id}")
+	public ModelAndView readUser(@PathVariable("id") User user) {
+		return new ModelAndView("read-user","user",user);
 	}
 	
-	@RequestMapping(path = "/update")
-	public @ResponseBody String updateUser(@RequestParam Integer id, String name, @RequestParam String email) {
-		User n = retrieveUser(id).get();
-		n.setName(name);
-		n.setEmail(email);
-		userRepository.save(n);
-		return "Updated";
-	}
+	// @RequestMapping(path = "/update")
+	// public @ResponseBody String updateUser(@RequestParam Integer id, String name, @RequestParam String email) {
+	// 	User n = retrieveUser(id).get();
+	// 	n.setName(name);
+	// 	n.setEmail(email);
+	// 	userRepository.save(n);
+	// 	return "Updated";
+	// }
 }
